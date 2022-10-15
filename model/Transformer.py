41738d2bb226139a09aeb1a6065147048faac9ca
yuzhenmao <harrymao666@gmail.com>
2022-10-15 00:01:40 -0700

    update

--- model/Transformer.py
+++ model/Transformer.py
@@ -133,7 +133,7 @@ class TransformerEncoder(nn.Module):
 
 class PositionalEncoding(nn.Module):
 
-    def __init__(self, d_model, max_len=5000):
+    def __init__(self, d_model, max_len=50):
         """
         Inputs
             d_model - Hidden dimensionality of the input.
@@ -141,23 +141,12 @@ class PositionalEncoding(nn.Module):
         """
         super().__init__()
 
-        # Create matrix of [SeqLen, HiddenDim] representing the positional encoding for max_len inputs
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
-
-        # register_buffer => Tensor which is not a parameter, but should be part of the modules state.
-        # Used for tensors that need to be on the same device as the module.
-        # persistent=False tells PyTorch to not add the buffer to the state dict (e.g. when we save the model)
-        self.register_buffer('pe', pe, persistent=False)
+        self.embed = nn.Embedding(max_len, d_model)
 
     def forward(self, x):
-        x = x + self.pe[:, :x.size(1)]
+        pe = torch.sin(self.embed(torch.arange(0, x.size(1), dtype=torch.long).cuda())).unsqueeze(0)
+        x = x + pe
         return x
-
 
 class TransformerPredictor(pl.LightningModule):
 
